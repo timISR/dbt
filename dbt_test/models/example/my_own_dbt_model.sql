ebc52f6bf403ce8bb247c24bfdefff5e1bad0a9f
Tim Liegmann <tim.liegmann@isr.de>
2022-11-24 14:04:17 +0100

    my second Commit

---
+++ dbt_test/models/example/my_own_dbt_model.sql
@@ -0,0 +1,12 @@
+{{ config(materialized='table') }}
+
+WITH FilmCat as(
+
+select COUNT(f.title) AS Anzahl, ctg.name  from film f
+JOIN film_category c ON c.film_id = f.film_id
+JOIN category ctg ON ctg.category_id = c.category_id
+GROUP BY ctg.name
+)
+
+SELECT * from FilmCat
+ORDER BY Anzahl DESC
